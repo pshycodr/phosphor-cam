8b531d53cf45645b4ed6bc5f59ee7ac1f29e4348
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-12-22 20:24:32 +0530

    feat(sw): add service worker with install event listner

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'phosphor-cam-v1.0.1'
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(async cache => {
+                console.log('[SW] log-1 ', cache)
+
+                const impAssets = ['/', '/index.html', '/manifest.json']
+
+                try {
+                    await cache.addAll(impAssets)
+                } catch (error) {
+                    console.log('faild to add impAssets: ', error)
+                }
+
+                const optionalAssets = [
+                    '/assets/logo.png',
+                    '/assets/favicon.png',
+                    '/assets/favicon.webp',
+
+                    '/assets/icons/icon-192x192.webp',
+                    '/assets/icons/icon-512x512.webp',
+                ]
+
+                await Promise.allSettled(
+                    optionalAssets.map(url => {
+                        console.log('[SW] log-2 ', url)
+
+                        cache.add(url).catch(err => {
+                            console.warn(`failed to cache ${url}: `, err)
+                        })
+                    }),
+                )
+            })
+            .then(() => self.skipWaiting())
+            .catch(err => {
+                console.log('failed to install cache: ', err)
+            }),
+    )
+})
